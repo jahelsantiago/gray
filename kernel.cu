869b859a09835d1868252a819f25b248eb5b02e7
Jahel_Leon <jleonro@unal.edu.co>
2022-06-01 21:22:22 -0500

    finished parallelization

--- kernel.cu
+++ kernel.cu
@@ -51,6 +51,14 @@ void getDeviceCharacteristics()
     std::cout << "deviceProp.computeMode: " << deviceProp.computeMode << std::endl;
 }
 
+/**
+ * @brief      { Converts the image to gray using CUDA }
+ *
+ * @param      image  The image 
+ * @param      width  The width of the image in pixels
+ * @param      height The height of the image in pixels
+ * @return 
+ */
 void ConvertImageToGrayCpu(unsigned char *image, int width, int height)
 {
     for (int row = 0; row < width; row++)
@@ -65,38 +73,39 @@ void ConvertImageToGrayCpu(unsigned char *image, int width, int height)
     }
 }
 
-__global__ void ConvertImageToGrayCuda(unsigned char *image, int width, int height)
-{
-    int row = blockIdx.x * blockDim.x + threadIdx.x;
-    int col = blockIdx.y * blockDim.y + threadIdx.y;
-
-    for (int i = 0; i <= 100; i++)
-    {
-        for (int j = 0; j <= 100; j++)
-        {
-            if (row < width && col < height)
-            {
-                Pixel *pixel = (Pixel *)&image[((row * width + col) + (i * blockDim.x) + (j * blockDim.y)) * 4];
-                unsigned char pixle_gray = (unsigned char)(0.299f * pixel->r + 0.587f * pixel->g + 0.114f * pixel->b);
-                pixel->r = pixel->g = pixel->b = pixle_gray;
-                pixel->a = 255;
-            }
-        }
-    }
-}
-
-__global__ void ConvertImageToGrayLinear(unsigned char *image, int width, int height)
+/**
+ * @brief      { Converts the image to gray using CUDA }
+ *
+ * @param      image  The image 
+ * @param      width  The width of the image in pixels
+ * @param      height The height of the image in pixels
+ * @param      iterations  The number of iterations that the kernel will run 
+ */
+__global__ void ConvertImageToGrayLinear(unsigned char *image, int width, int height, int iterations)
 {
     int pixel_index = threadIdx.x + blockIdx.x * blockDim.x;
-    if (pixel_index < width * height)
+    // get the total number of threads
+    int total_threads = blockDim.x * gridDim.x;
+    for (int i = 0; i < iterations; i++)
     {
-        Pixel *pixel = (Pixel *)&image[pixel_index * 4];
-        unsigned char pixle_gray = (unsigned char)(0.299f * pixel->r + 0.587f * pixel->g + 0.114f * pixel->b);
-        pixel->r = pixel->g = pixel->b = pixle_gray;
-        pixel->a = 255;
+        if (pixel_index < width * height)
+        {
+            Pixel *pixel = (Pixel *)&image[pixel_index * 4];
+            unsigned char pixle_gray = (unsigned char)(0.299f * pixel->r + 0.587f * pixel->g + 0.114f * pixel->b);
+            pixel->r = pixel->g = pixel->b = pixle_gray;
+            pixel->a = 255;
+        }
+        pixel_index += total_threads;
     }
 }
 
+/**
+ * @brief Run the algorithm on the CPU
+ * 
+ * @param image - the image to be converted
+ * @param width - the width of the image
+ * @param height - the height of the image 
+ */
 void runOnCpu(unsigned char *imageData, int width, int height)
 {
     auto start = std::chrono::high_resolution_clock::now();
@@ -109,6 +118,47 @@ void runOnCpu(unsigned char *imageData, int width, int height)
     std::cout << "Time taken by function on Cpu: " << elapsed.count() << " seconds" << std::endl;
 }
 
+/**
+ * @brief Run the algorithm on the GPU
+ * 
+ * @param imageData - image data to be processed on GPU device (CUDA) 
+ * @param width - image width in pixels 
+ * @param height - image height in pixels
+ */
+void runOnDevice(unsigned char *imageData, int width, int height)
+{
+    // allocate memory for the image on the device
+    unsigned char *imageDataDevice = nullptr;
+    cudaMalloc(&imageDataDevice, width * height * 4);
+    assert(imageDataDevice != nullptr);
+    // copy the image to the device
+    cudaMemcpy(imageDataDevice, imageData, width * height * 4, cudaMemcpyHostToDevice);
+
+    // process the image on the device
+    auto start_gpu = std::chrono::high_resolution_clock::now();
+
+    int blocks = 1;
+    int threads_per_block = 32;
+
+    int total_pixel = width * height;
+    int iterations = total_pixel / (blocks * threads_per_block);
+    if (total_pixel % (blocks * threads_per_block) != 0)
+        iterations++;
+
+    ConvertImageToGrayLinear<<<blocks, threads_per_block>>>(imageDataDevice, width, height, iterations);
+    cudaDeviceSynchronize();
+
+    auto end_gpu = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed_gpu = end_gpu - start_gpu;
+    std::cout << "Time taken by function on Device: " << elapsed_gpu.count() << " seconds" << std::endl;
+
+    // copy the image back to the host
+    cudaMemcpy(imageData, imageDataDevice, width * height * 4, cudaMemcpyDeviceToHost);
+    // free the device memory
+    cudaFree(imageDataDevice);
+}
+
+
 int main()
 {
 
@@ -118,59 +168,19 @@ int main()
     // Load image
     int width, height, channels;
     unsigned char *imageData = stbi_load(path, &width, &height, &channels, 4);
-
-    // print the value of the width, height and channels
-    std::cout << "width: " << width << std::endl;
-    std::cout << "height: " << height << std::endl;
-    std::cout << "channels: " << channels << std::endl;
-
     if (imageData == nullptr)
     {
         std::cerr << "Failed to load image" << std::endl;
         return -1;
     }
 
-    // validate image size
-    if (width % 32 || height % 32)
-    {
-        std::cerr << "Image size must be multiple of 32" << std::endl;
-        return -1;
-    }
-
     // runOnCpu(imageData, width, height);
-
-    // convert image to gray scale with Cuda --------------------------------------------------
-
-    // allocate memory for the image on the device
-    std::cout << "Copying date to device ...";
-    unsigned char *imageDataDevice = nullptr;
-    cudaMalloc(&imageDataDevice, width * height * 4);
-    assert(imageDataDevice != nullptr);
-    // copy the image to the device
-    cudaMemcpy(imageDataDevice, imageData, width * height * 4, cudaMemcpyHostToDevice);
-    std::cout << "Done" << std::endl;
-
-    // process the image on the device
-    auto start_gpu = std::chrono::high_resolution_clock::now();
-    std::cout << "Processing on device...";
-
-    ConvertImageToGrayLinear<<<102400, 1024>>>(imageDataDevice, width, height);
-    std::cout << "Done" << std::endl;
-    auto end_gpu = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed_gpu = end_gpu - start_gpu;
-    std::cout << "Time taken by function on Device: " << elapsed_gpu.count() << " seconds" << std::endl;
-
-    // copy the image back to the host
-    std::cout << "Copying data back to host...";
-    cudaMemcpy(imageData, imageDataDevice, width * height * 4, cudaMemcpyDeviceToHost);
-    std::cout << "Done" << std::endl;
+    runOnDevice(imageData, width, height);
 
     // write image back to disk
-    std::cout << "Writing png to disk...";
     stbi_write_png("lena_out.png", width, height, 4, imageData, 4 * width);
-    std::cout << "Done" << std::endl;
 
-    // close image
+    // free memory
     stbi_image_free(imageData);
 
     return 0;
